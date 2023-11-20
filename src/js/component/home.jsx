d272f8b219d401ce57671ecdecea3f1a4ee3203a
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-20 23:57:49 +0000

    Api synch

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 function Home() {
 	const [tasks, setTasks] = useState([]);
+
 	const deleteTask = (indexToDelete) => {
 		setTasks((prevTasks) => prevTasks.filter((task, index) => index !== indexToDelete));
 	};
+
+	useEffect(() => {
+		fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json"
+			}
+		})
+			.then(resp => {
+				console.log(resp.ok); // will be true if the response is successfull
+				console.log(resp.status); // the status code = 200 or code = 400 etc.
+				console.log(resp.text()); // will try return the exact result as string
+				return resp.json(); // (returns promise) will try to parse the result as json as return a promise that you can .then for results
+			})
+			.then(data => {
+				//here is where your code should start after the fetch finishes
+				setTasks(data)
+				console.log(data); //this will print on the console the exact object received from the server
+			})
+			.catch(error => {
+				//error handling
+				console.log(error);
+			});
+	}, []);
+
+
+
+
+
 
 	return (
 		<div className="container">
