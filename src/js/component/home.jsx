f7558e0248d3104477757d2efb6b7a42630d1d87
Santi-Quijano <108496719+Santi-Quijano@users.noreply.github.com>
2023-11-29 20:32:23 +0000

    Finished Project

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,94 +1,83 @@
 import React, { useState, useEffect } from "react";
 
 function Home() {
-	const [tasks, setTasks] = useState([]);
+    const [tasks, setTasks] = useState([]);
 
-	let nextTaskId = 1;
+    const deleteTask = (taskId) => {
+        const updatedTasks = tasks.filter((task) => task.id !== taskId)
+        setTasks(updatedTasks);
+        updateTasksOnServer(updatedTasks);
+    };
 
-	const fetchData = () => {
-		fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
-			headers: {
-				"Content-Type": "application/json",
-			},
-		})
-			.then((response) => response.json())
-			.then((data) => {
-				setTasks(data);
-			})
-			.catch((error) => {
-				console.log(error);
-			});
-	};
-	useEffect(() => {
-		fetchData();
-	}, []);
+    const updateTasksOnServer = (updatedTasks) => {
+        fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(updatedTasks),
+        })
+            .then((resp) => resp.json())
+            .then((data) => console.log(data))
+            .catch((error) => console.log(error));
+    };
 
-	const addTask = (newTask) => {
-		const newTaskWithId = { id: nextTaskId++, task: newTask };
-		fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
-			method: "GET",
-			headers: {
-				"Content-Type": "application/json",
+    const addTask = (taskLabel) => {
+        const newTask = { id: tasks.length + 1, label: taskLabel, done: false };
+        const updatedTasks = [...tasks, newTask];
+        setTasks(updatedTasks);
+        updateTasksOnServer(updatedTasks);
+    }
 
-			},
-			body: JSON.stringify([...tasks, newTaskWithId]),
-		})
-			.then(() => fetchData())
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    const eraseAllTasks = () => {
+        setTasks([]);
+        updateTasksOnServer([]);
+    };
 
-	const deleteTask = (taskId) => {
-		const updatedTasks = tasks.filter((task) => task.id !== taskId);
-		fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
-			method: PUT,
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(updatedTasks),
-		})
-			.then(() => setTasks(updatedTasks))
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    useEffect(() => {
+        fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                else return response.json();
+            })
+            .then((data) => setTasks(data))
+            .catch((error) => console.log(error));
+    }, []);
 
-	const deleteAllTasks = () => {
-		fetch("https://playground.4geeks.com/apis/fake/todos/user/Santi-Quijano", {
-			method: "PUT",
-			headers: {
-				"Content-Type": "applications/json",
-			},
-			body: JSON.stringify([]),
-		})
-			.then(() => fetchData())
-			.catch((error) => {
-				console.log(error);
-			});
-	};
+    return (
+        <div className="container">
+            <h1 className="text-muted">todos</h1>
+            <input type="text" placeholder="What needs to be done?" onKeyDown={(e) => {
+                if (e.code === "Enter") {
+                    addTask(e.target.value);
+                    e.target.value = ''; //To Clear Input
+                }
+            }}
+            />
 
-	return (
-		<div className="container">
-			<h1 className="text-muted">todos</h1>
-			<input type="text" placeholder="What needs to be done?" onKeyDown={(e) => { if (e.code === "Enter") addTask(e.target.value); }} />
-			<ul>
-				{tasks.length === 0 && <li>No tasks, add a task pressing Enter</li>}
-				{tasks.map((task) => (
-					<li key={task.id}>
-						<p>{task.task}</p>
-						<button className="erase" onClick={() => deleteTask(task.id)}>X</button>
-					</li>
-				))}
-				<li className="list-group-item fontSizeSmall">{tasks.length === 1 ? "1 item" : `${tasks.length} items`}</li>
-			</ul>
-
-		</div>
-	);
+            <ul>
+                {tasks.length === 0 && <li>No tasks, add a task pressing Enter.</li>}
+                {tasks.map((task) => (
+                    <li key={task.id}>
+                        <p>{task.label}</p>
+                        <button className="erase" onClick={() => deleteTask(task.id)}>
+                            X
+                        </button>
+                    </li>
+                ))}
+                <li className="list-group-item fontSizeSmall">
+                    {tasks.length === 1 ? "1 item" : `${tasks.length} items`}
+                </li>
+            </ul>
+            <button className="deleteEverything" onClick={eraseAllTasks}>Delete Everything</button>
+        </div>
+    )
 }
 export default Home;
-
-
-
-
-
